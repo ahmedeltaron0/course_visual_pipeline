9f6cb1bf9cc2f487842313099a932d8a84fd6a97
Omar Atef <135141854+o2204@users.noreply.github.com>
2025-12-16 16:23:55 +0200

    Delete exceptions directory

--- exceptions/service_exception.py
+++
@@ -1,7 +0,0 @@
-from exceptions.custom_exception import CustomException
-
-
-class ServiceException(CustomException):
-    def __init__(self, status_code: int = 500, detail: str = "Service error", additional_info: dict = None):
-        super().__init__(status_code=status_code, detail=detail,
-                         exception_type="ServiceException", additional_info=additional_info)
